b26b258a29ef06b3c491386c5e5c93fccd9d2187
divinagracia <vincentdivinagracia1@gmail.com>
2023-04-24 11:15:31 +0800

    Refactored mobile

--- theme/js/script.js
+++ theme/js/script.js
@@ -65,23 +65,16 @@ $(window).resize(function () {
 
 function setMobile() {
     if (window.matchMedia("screen and (max-width: 767px").matches) {
-        $(".nav-header").css("display", "none");
-        $(".site-header").css("display", "none");
-        $(".nav-bar").css("display", "none");
         $(".mobile-nav").prependTo(".navigation");
-        $(".mobile-nav").css("display", "flex");
         $(".product-img-container").prependTo(".product-content");
         $("#wrapper").attr("src", "theme/img/footer-banner-2.png");
         $(".nav-links").prependTo(".navigation");
         $(".menu-link, .page-footer-container").addClass("mobile");
     } else {
-        $(".nav-header").css("display", "block");
-        $(".site-header").css("display", "flex");
-        $(".nav-bar").css("display", "block");
-        $(".mobile-nav").css("display", "none");
         $(".product-img-container").before($(".selection-container"));
         $("#wrapper").attr("src", "theme/img/footer-banner.png");
         $(".nav-categories").after($(".nav-links"));
+
         $(".menu-link, .page-footer-container").removeClass("mobile");
         $(
             ".menu-link ul, .page-footer-container li, .page-footer-container > div div, .page-footer-container p"
